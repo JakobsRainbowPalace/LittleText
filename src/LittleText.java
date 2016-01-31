218048cd1ce440729c72d0459f5d626c1ede064f
JakobsRainbowPalace <jakobrieke.dienstlich@gmail.com>
2016-01-31 23:36:26 +0100

    LittleText.java
    contains the main file to start the programm

---
+++ src/LittleText.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+
+public class LittleText {
+	
+	// Little Text is a small Texteditor, written by Jakob Rieke
+
+	public static void main(String[] args) {
+		
+		Application.launch(LtWindow.class, args);
+	}
+	
+}
